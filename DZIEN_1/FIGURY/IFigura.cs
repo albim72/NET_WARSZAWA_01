ff1d3387b4bc08617428b1aa0ce6e138ba016c3e
albim72 <marcin.albiniak72@gmail.com>
2023-01-23 12:31:02 +0100

    Create IFigura.cs

---
+++ DZIEN_1/FIGURY/IFigura.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figury
+{
+    interface IFigura
+    {
+        double policz_pole();
+        double policz_obw();
+
+        //String opis(String op) { return $"informacja:{op}"; }
+    }
+}
