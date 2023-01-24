5083d8183d58695226ddfc2d26490448e1dc9cdb
albim72 <marcin.albiniak72@gmail.com>
2023-01-24 14:12:39 +0100

    Create ConcreteProductA2.cs

---
+++ DZIEN_2/ABSTACTFACTORY/ConcreteProductA2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabrykaAbstrakcyjna
+{
+    public class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "wynik tworzenia produktu A2";
+        }
+    }
+}
