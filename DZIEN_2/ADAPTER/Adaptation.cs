fc0c1b9c18183976aa0b9c7a9f71877cce977094
albim72 <marcin.albiniak72@gmail.com>
2023-01-24 12:16:58 +0100

    Create Adaptation.cs

---
+++ DZIEN_2/ADAPTER/Adaptation.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class Adaptation
+    {
+        public string GetSpecificRequest()
+        {
+            return "nietypowa odpowiedź...";
+        }
+    }
+}
