a984388f7ef204455f25f48b39a0a830553a73a4
albim72 <marcin.albiniak72@gmail.com>
2023-01-24 11:53:34 +0100

    Create Product.cs

---
+++ DZIEN_2/BUILDER/Product.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budowniczy
+{
+    public class Produkt
+    {
+        private List<object> _parts = new List<object>();
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+            for(int i=0;i<this._parts.Count;i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+            str = str.Remove(str.Length-2);
+            return $"Części produktu: {str}\n";
+        }
+    }
+}
